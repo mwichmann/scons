f529e3e424669ffa33ec74a758a934c50e04209e
Mats Wichmann <mats@linux.com>
2021-06-29 07:14:31 -0600

    Reformatted testcases
    Signed-off-by: Mats Wichmann <mats@linux.com>

--- test/AddOption/args-and-targets.py
+++ test/AddOption/args-and-targets.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #
-# __COPYRIGHT__
+# MIT License
+#
+# Copyright The SCons Foundation
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -20,9 +22,6 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-
-__revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
 
 """
 Verify that when an option is specified which takes args,
@@ -33,19 +32,24 @@ import TestSCons
 
 test = TestSCons.TestSCons()
 
-test.write('SConstruct', """\
+test.write(
+    'SConstruct',
+    """\
 env = Environment()
-AddOption('--extra',
-          nargs=1,
-          dest='extra',
-          action='store',
-          type='string',
-          metavar='ARG1',
-          default=(),
-          help='An argument to the option')
+AddOption(
+    '--extra',
+    nargs=1,
+    dest='extra',
+    action='store',
+    type='string',
+    metavar='ARG1',
+    default=(),
+    help='An argument to the option',
+)
 print(str(GetOption('extra')))
 print(COMMAND_LINE_TARGETS)
-""")
+""",
+)
 
 # arg using =
 test.run('-Q -q --extra=A TARG', status=1, stdout="A\n['TARG']\n")
